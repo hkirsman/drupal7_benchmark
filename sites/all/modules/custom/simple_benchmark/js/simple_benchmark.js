72519b786e6b02c5d27fe20ee07a95b9efa22ea9
Hannes Kirsman <hannes.kirsman@wunderkraut.com>
2016-01-20 10:08:25 +0200

    Simpel Benchmark: add os and php version, help message and change layout.

--- sites/all/modules/custom/simple_benchmark/js/simple_benchmark.js
+++ sites/all/modules/custom/simple_benchmark/js/simple_benchmark.js
@@ -6,28 +6,41 @@
 (function($) {
   var timeBest = 0;
   var timeWorst = 0;
-  //var timesAll = [];
-  //var testCount = 0;
+  var timesAll = [];
+  var testCount = 0;
 
   var ping = function() {
     var now = new Date().getTime();
     $.get('/admin/config', function(data) {
       var then = new Date().getTime();
       var time = then - now;
-      //timesAll[timesAll.length] = time;
-      $('.simple-benchmark .log').prepend('<div>' + time + '</div>');
+      timesAll[timesAll.length] = time;
+
+      $('.simple-benchmark .log .content').prepend('<div>' + time + '</div>');
       if (timeBest === 0 || time < timeBest) {
         timeBest = time;
       }
       if (timeWorst === 0 || time > timeWorst) {
         timeWorst = time;
       }
+
+      var totalTime = 0;
+      $.each(timesAll, function(index, value) {
+        totalTime += value;
+      });
+      var timesAverage = totalTime / timesAll.length;
+
       $('.simple-benchmark .current').html(
-        '<strong>' + Drupal.t('Best') + '</strong>' + ': ' +  timeBest/* + '<br>' +
-        '<strong>' + Drupal.t('Worst') + '</strong>' + ': ' +  timeWorst*/
+        '<strong>' + Drupal.t('Best') + '</strong>' + ': ' +  timeBest + '<br>' +
+        '<strong>' + Drupal.t('Average') + '</strong>' + ': ' +  Math.round(timesAverage)
+        //'<strong>' + Drupal.t('Worst') + '</strong>' + ': ' +  timeWorst*/
       );
 
-      setTimeout(ping, 500);
+      testCount++;
+
+      if (testCount < 20) {
+        setTimeout(ping, 500);
+      }
     });
   };
 
--- sites/all/modules/custom/simple_benchmark/theme/simple_benchmark_page.tpl.php
+++ sites/all/modules/custom/simple_benchmark/theme/simple_benchmark_page.tpl.php
@@ -7,8 +7,12 @@
 ?>
 
 <div class="simple-benchmark">
-  <div class="log"></div>
-  <div class="current">
-
+  <div class="header">
+    <?php print "PHP version: " . $php_version; ?><br>
+    <?php print "Platform: " . $platform; ?></div>
+  <div class="current"></div>
+  <div class="log">
+    <div class="header"><strong>All pings:</strong></div>
+    <div class="content"></div>
   </div>
 </div>
